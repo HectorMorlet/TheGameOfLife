84bcd6b0108db4e0111212e9238b995edacd7514
MagneticHamster <hector.morlet@gmail.com>
2014-04-21 13:00:57 +0800

    Completed first item on plan

--- theGameOfLife.c
+++ theGameOfLife.c
@@ -13,6 +13,49 @@
 
 #include <math.h>
 #include <ncurses.h>
+#include <unistd.h>
 
 #include "theGameOfLife.h"
 
+
+#define TRUE 1
+#define FALSE 0
+
+
+void initCurses();
+
+
+// .oO0-------------------------------------------------------0Oo. //
+// ----------------------------- Main ---------------------------- //
+// .oO0-------------------------------------------------------0Oo. //
+
+int main(int argc, char *argv[]) {
+   initCurses();
+
+   int x = 0;
+   while(x < 60) {
+      move(5, x);
+      addch('#');
+      move(5, x-1);
+      addch(' ');
+      usleep(50000);
+      refresh();
+      x++;
+   }
+
+   endwin();
+
+   return EXIT_SUCCESS;
+}
+
+
+// .oO0--------------------------------------------------------0Oo. //
+// ------------------ Starting and ending curses ------------------ //
+// .oO0--------------------------------------------------------0Oo. //
+
+void initCurses() {
+   initscr();
+   //cbreak();
+   noecho();
+   //keypad(stdscr, TRUE);
+}
---
+++ theGameOfLifeTester.c
@@ -0,0 +1,21 @@
+
+//
+// theGameOfLifeTester.h
+// Tester of theGameOfLife.c
+// By Hector Morlet
+// 19 April 2014
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "theGameOfLife.h"
+
+
+int main(int argc, char *argv[]) {
+   // Use testing functions
+   return EXIT_SUCCESS;
+}
+
+// Write testing functions here
