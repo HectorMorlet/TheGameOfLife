1e5975df73c111a1bf4f024ce2762e3659435de4
MagneticHamster <hector.morlet@gmail.com>
2014-04-21 20:54:51 +0800

    Made it work with built in input

--- theGameOfLife.c
+++ theGameOfLife.c
@@ -21,19 +21,30 @@
 #define TRUE 1
 #define FALSE 0
 
-#define liveCellChar '#'
-#define deadCellChar ' '
+#define SIZE_OF_A_CELL 10
 
-#define delay 50000
+#define LIVE_CELL_CHAR '#'
+#define DEAD_CELL_CHAR ' '
 
+#define DELAY_PER_GENERATION 500000
+#define ITERATIONS 1000
+
+#define NEIGHBOURS_PER_CELL 8
+#define MAX_DISTANCE_TO_SURROUNDING_CELL 1
+
+#define FIRST_NUMBER_OF_SURROUNDING_LIVE_CELLS 2
+#define SECOND_NUMBER_OF_SURROUNDING_LIVE_CELLS 3
+#define NUMBER_OF_CELLS_FOR_DEAD_TO_LIVING 3
+
+
+void iterateGeneration(cell *map, int width, int height);
 
 cell *createMap(cell *map, int width, int height);
 void renderMap(cell *map, int width, int height);
 
-void iterateGeneration();
-void isAlive();
-
-void iterateGeneration();
+cell *updateMap(cell *map, int width, int height);
+int cellNeighbours(cell *map, int cellPosition, int width);
+cell *updateCell(cell *map, int cellPosition, int neighbours);
 
 void initCurses();
 
@@ -45,15 +56,17 @@ void initCurses();
 int main(int argc, char *argv[]) {
    initCurses();
 
+   // Finding terminal dimentions
    int height;
    int width;
    getmaxyx(stdscr, height, width);
-   cell *map = malloc(100000); // Work the size of thew map out
+
+   // Creating map
+   cell *map = malloc(width * height * SIZE_OF_A_CELL);
    map = createMap(map, width, height);
 
-   renderMap(map, width, height);
-   refresh();
-   usleep(5000000);
+   // Rendering
+   iterateGeneration(map, width, height);
 
    endwin();
 
@@ -61,12 +74,82 @@ int main(int argc, char *argv[]) {
 }
 
 
+// .oO0-------------------------------------------------------0Oo. //
+// ---------------- Iterating Through Generations ---------------- //
+// .oO0-------------------------------------------------------0Oo. //
+
+void iterateGeneration(cell *map, int width, int height) {
+   usleep(DELAY_PER_GENERATION);
+
+   int i = 0;
+   while (i < ITERATIONS) {
+      renderMap(map, width, height);
+      map = updateMap(map, width, height);
+
+      i++;
+      usleep(DELAY_PER_GENERATION);
+   }
+}
+
+
+// .oO0-------------------------------------------------------0Oo. //
+// ---------------- Creating and Rendering the Map --------------- //
+// .oO0-------------------------------------------------------0Oo. //
+
+void renderMap(cell *map, int width, int height) {
+   int y = 0;
+   while (y < height) {
+      int x = 0;
+      while (x < width) {
+         move(y, x);
+         if (map[y * width + x].lifeStatus) {
+            addch(LIVE_CELL_CHAR);
+         } else {
+            addch(DEAD_CELL_CHAR);
+         }
+         x++;
+      }
+      y++;
+   }
+   refresh();
+}
+
+
 cell *createMap(cell *map, int width, int height) {
    int i = 0;
    while (i < height * width) {
-      map[i].changeNextGeneration = FALSE;
+      map[i].lifeStatNextGeneration = FALSE;
       map[i].lifeStatus = FALSE;
-      if (i == 20 || i == 21 || i == 360 || i == 61) {
+      if (i == 23 || 
+         i == 18 || 
+         i == 20 || 
+         i == 21 || 
+         i == 99 || 
+         i == 100 ||
+         i == 101 ||
+         i == 180 ||
+         i == 181 ||
+         i == 105 ||
+         i == 129 ||
+         i == 185 ||
+         i == 123 ||
+         i == 483 ||
+         i == 970 ||
+         i == 264 ||
+         i == 957 ||
+         i == 274 ||
+         i == 265 ||
+         i == 287 ||
+         i == 241 ||
+         i == 207 ||
+         i == 396 ||
+         i == 69 ||
+         i == 185 ||
+         i == 106 ||
+         i == 16 ||
+         i == 96 ||
+         i == 98 ||
+         i == 97) {
          map[i].lifeStatus = TRUE;
       }
       i++;
@@ -76,33 +159,71 @@ cell *createMap(cell *map, int width, int height) {
 }
 
 
-void renderMap(cell *map, int width, int height) {
+// .oO0-------------------------------------------------------0Oo. //
+// ---------------- Updating to the New Generation --------------- //
+// .oO0-------------------------------------------------------0Oo. //
 
-   int y = 0;
-   while (y < height) {
-      int x = 0;
-      while (x < width) {
-         move(y, x);
-         if (map[y * width + x].lifeStatus) {
-            addch(liveCellChar);
-         }
-         x++;
-      }
-      y++;
-   }
-}
-
-
-void iterateGeneration() {
+cell *updateMap(cell *map, int width, int height) {
    int i = 0;
-   while (i < 1000) {
-      
+   while (i < (width * height)) {
+      int neighbours = cellNeighbours(map, i, width);
+      map = updateCell(map, i, neighbours);
+      i++;
    }
+   i = 0;
+   while (i < (height * width)) {
+      map[i].lifeStatus = map[i].lifeStatNextGeneration;
+      i++;
+   }
+
+   return map;
 }
 
 
-void isAlive() {
+int cellNeighbours(cell *map, int cellPosition, int width) {
+   // Finding the cell's neighbours
+   cell cellNeighbours[NEIGHBOURS_PER_CELL] = {
+      map[cellPosition - width - MAX_DISTANCE_TO_SURROUNDING_CELL],
+      map[cellPosition - width],
+      map[cellPosition - width + MAX_DISTANCE_TO_SURROUNDING_CELL],
+      map[cellPosition - MAX_DISTANCE_TO_SURROUNDING_CELL],
+      map[cellPosition + MAX_DISTANCE_TO_SURROUNDING_CELL],
+      map[cellPosition + width - MAX_DISTANCE_TO_SURROUNDING_CELL],
+      map[cellPosition + width],
+      map[cellPosition + width + MAX_DISTANCE_TO_SURROUNDING_CELL]
+   };
 
+   // Finding how many neighbours are alive
+   int neighbourCount = 0;
+   int i = 0;
+   while (i < NEIGHBOURS_PER_CELL) {
+      if (cellNeighbours[i].lifeStatus == TRUE) {
+         neighbourCount++;
+      }
+      i++;
+   }
+
+   return neighbourCount;
+}
+
+
+cell *updateCell(cell *map, int cellPosition, int neighbours) {
+   if (map[cellPosition].lifeStatus == TRUE) {
+      if (neighbours == FIRST_NUMBER_OF_SURROUNDING_LIVE_CELLS || 
+         neighbours == SECOND_NUMBER_OF_SURROUNDING_LIVE_CELLS) {
+         map[cellPosition].lifeStatNextGeneration = TRUE;
+      } else {
+         map[cellPosition].lifeStatNextGeneration = FALSE;
+      }
+   } else {
+      if (neighbours == NUMBER_OF_CELLS_FOR_DEAD_TO_LIVING) {
+         map[cellPosition].lifeStatNextGeneration = TRUE;
+      } else {
+         map[cellPosition].lifeStatNextGeneration = FALSE;
+      }
+   }
+
+   return map;
 }
 
 
--- theGameOfLife.h
+++ theGameOfLife.h
@@ -8,6 +8,6 @@
 
 
 typedef struct {
-   int changeNextGeneration;
+   int lifeStatNextGeneration;
    int lifeStatus;
 } cell;
