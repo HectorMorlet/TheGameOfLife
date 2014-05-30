96a86b9624768c3b98134b59ddfe7555033e6b8e
MagneticHamster <hector.morlet@gmail.com>
2014-05-30 12:02:56 +0800

    Changed colours around

--- theGameOfLife.c
+++ theGameOfLife.c
@@ -52,15 +52,6 @@
 #define POINTS_IN_INTRO_TEXT 364
 
 
-int *getInput();
-
-void iterateGeneration(cell *map, int width, int height);
-
-void renderMap(cell *map, int width, int height);
-
-void initCurses();
-
-
 // .oO0-------------------------------------------------------0Oo. //
 // ----------------------------- Main ---------------------------- //
 // .oO0-------------------------------------------------------0Oo. //
@@ -392,11 +383,11 @@ void initCurses() {
 
    start_color();
    init_pair(0, COLOR_BLACK, COLOR_BLACK);
-   init_pair(1, COLOR_WHITE, COLOR_BLACK);
+   init_pair(6, COLOR_WHITE, COLOR_BLACK);
    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
    init_pair(3, COLOR_GREEN, COLOR_BLACK);
-   init_pair(4, COLOR_CYAN, COLOR_BLACK);
+   init_pair(7, COLOR_CYAN, COLOR_BLACK);
    init_pair(5, COLOR_BLUE, COLOR_BLACK);
-   init_pair(6, COLOR_RED, COLOR_BLACK);
-   init_pair(7, COLOR_MAGENTA, COLOR_BLACK);
+   init_pair(1, COLOR_RED, COLOR_BLACK);
+   init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
 }
--- theGameOfLife.h
+++ theGameOfLife.h
@@ -13,13 +13,19 @@ typedef struct _cell {
    int color;
 } cell;
 
+int *getInput();
+
+void iterateGeneration(cell *map, int width, int height);
 
 int *translateIntroText(int *introArray,
                         char *introText,
                         int width);
 
+void renderMap(cell *map, int width, int height);
 cell *createMap(cell *map, int width, int height, int *input);
 
 cell *updateMap(cell *map, int width, int height);
 int cellNeighbours(cell *map, int cellPosition, int width);
 cell *updateCell(cell *map, int cellPosition, int neighbours);
+
+void initCurses();
